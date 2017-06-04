e3d08a49f6793d9c899a13fe2093e1899f549fb7
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-06-05 00:20:37 +0200

    Split up version assertions into multiple tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,13 +1,37 @@
 from mountapi import version
 
 
-def test_version_data_exists():
-    assert version.__name__
-    assert version.__description__
-    assert version.__url__
-    assert version.__version__
-    assert version.__build__
-    assert version.__author__
-    assert version.__author_email__
-    assert version.__license__
-    assert version.__copyright__
+def test_version_contains_name():
+    assert hasattr(version, '__name__')
+
+
+def test_version_contains_description():
+    assert hasattr(version, '__description__')
+
+
+def test_version_contains_url():
+    assert hasattr(version, '__url__')
+
+
+def test_version_contains_version():
+    assert hasattr(version, '__version__')
+
+
+def test_version_contains_build():
+    assert hasattr(version, '__build__')
+
+
+def test_version_contains_author():
+    assert hasattr(version, '__author__')
+
+
+def test_version_contains_author_email():
+    assert hasattr(version, '__author_email__')
+
+
+def test_version_contains_license():
+    assert hasattr(version, '__license__')
+
+
+def test_version_contains_copyright():
+    assert hasattr(version, '__copyright__')
