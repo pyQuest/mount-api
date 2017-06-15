67d810959f1ab1c41f40913583f8cd22f71ec55a
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-06-16 00:36:36 +0200

    Use DefaultSettings in Application tests
    Store tests cases in test functions' local scopes instead of global

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ import pytest
 
 from mountapi.core.app import Application
 from mountapi.core import exceptions
-from mountapi.core.settings import AbstractSettings
+from mountapi.core.settings import DefaultSettings
 
 
 class InvalidRouter:
@@ -12,30 +12,20 @@ class InvalidRouter:
         self.router = router
 
 
-class TestSettings(AbstractSettings):
-    debug = False
-    hostname = 'localhost'
-    port = 8000
-    router = 'mountapi.routing.Router'
-    runner = 'mountapi.runners.Runner'
-
-
-class InvalidRunnerTestSettings(AbstractSettings):
-    debug = False
-    hostname = 'localhost'
-    port = 8000
-    router = 'mountapi.routing.Router'
-    runner = 'tests.test_app.InvalidRouter'
-
-
 @patch('mountapi.runners.Runner.run')
-def test_app_valid_with_valid_input(run_simple_mock):
+def test_app_valid_with_empty_routes(run_simple_mock):
+    class TestSettings(DefaultSettings):
+        pass
+
     app = Application(TestSettings, routes=[])
     app.run()
 
 
 @patch('mountapi.runners.Runner.run')
 def test_app_not_run_without_valid_runner(run_simple_mock):
+    class InvalidRunnerTestSettings(DefaultSettings):
+        runner = 'tests.test_app.InvalidRouter'
+
     with pytest.raises(exceptions.InvalidRunner):
         app = Application(InvalidRunnerTestSettings, routes=[])
         app.run()
