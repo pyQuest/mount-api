c0d344acc008cb2c98333e6ada6912c10729626f
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-06-19 01:02:06 +0200

    Add get allowed methods without class instance

--- mountapi/endpoints.py
+++ mountapi/endpoints.py
@@ -1,11 +1,16 @@
 import abc
+from typing import Set
 
 HTTP_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS']
 
 
 class AbstractEndpoint(metaclass=abc.ABCMeta):
     @property
-    def accepted_methods(self) -> set:
+    def allowed_methods(self) -> Set[str]:
+        return self.get_allowed_methods()
+
+    @classmethod
+    def get_allowed_methods(cls) -> Set[str]:
         return {
-            method for method in HTTP_METHODS if hasattr(self, method.lower())
+            method for method in HTTP_METHODS if hasattr(cls, method.lower())
         }
